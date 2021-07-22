a4a807926392b56b78557fca8dfcc92ff673d988
nottherealedmund <nottherealedmund@gmail.com>
2021-07-22 17:58:17 +0800

    Add a functional search bar to search for doujins by title

--- mydoujinlist/home.php
+++ mydoujinlist/home.php
@@ -26,49 +26,61 @@ Available at: https://codeshack.io/secure-login-system-php-mysql/
   		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
   		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 		<link href="main.css" rel="stylesheet" type="text/css">
+		<style> 
+			::placeholder {
+				font-size: 18px;
+				color: #e9658d;
+			}
+			.mySearchButton {
+				cursor: pointer;
+				background: #e9658d;
+				border: 0;
+				color: white;
+			  	padding: 3px;
+			  	border-radius: 5px;
+			}
+			.grid-container { 
+				display: grid;
+				grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+				grid-gap: 10px;
+				align-items: stretch;
+				width: 1200px;
+				margin: auto;
+				padding-bottom: 150px;
+			}
+			.doujin {
+				border: 1px solid #ccc;
+				box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
+				height: 440px;
+				border-radius: 10px;
+				/*Required to make button stay at the bottom*/
+				position: relative;
+			}
+			.doujin img {
+			  	max-width: 100%;
+			  	border-top-left-radius: 10px;
+				border-top-right-radius: 10px;
+			}
+			.info {
+			  	text-align: center;
+			  	font-size: 14px;
+			  	font-weight: 700;
+			}
+			.myButton {
+			  	cursor: pointer;
+				background: #e9658d;
+				border: 0;
+				color: white;
+			  	padding: 10px;
+			  	width: 100%;
+			  	border-bottom-left-radius: 10px;
+				border-bottom-right-radius: 10px;
+			  	/*Required to make button stay at the bottom*/
+			  	position: absolute;
+			  	bottom: 0;
+			}
+		</style>
 	</head>
-	<style> 
-		.grid-container { 
-			display: grid;
-			grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-			grid-gap: 10px;
-			align-items: stretch;
-			width: 1200px;
-			margin: auto;
-			padding-bottom: 150px;
-		}
-		.doujin {
-			border: 1px solid #ccc;
-			box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
-			height: 440px;
-			border-radius: 10px;
-			/*Required to make button stay at the bottom*/
-			position: relative;
-		}
-		.doujin img {
-		  	max-width: 100%;
-		  	border-top-left-radius: 10px;
-			border-top-right-radius: 10px;
-		}
-		.info {
-		  	text-align: center;
-		  	font-size: 14px;
-		  	font-weight: 700;
-		}
-		.myButton {
-		  	cursor: pointer;
-			background: #e9658d;
-			border: 0;
-			color: white;
-		  	padding: 10px;
-		  	width: 100%;
-		  	border-bottom-left-radius: 10px;
-			border-bottom-right-radius: 10px;
-		  	/*Required to make button stay at the bottom*/
-		  	position: absolute;
-		  	bottom: 0;
-		}
-	</style>
 	<body class="loggedin">
 		<?php require 'navbar.html'; ?>
 		
@@ -91,22 +103,60 @@ Available at: https://codeshack.io/secure-login-system-php-mysql/
 			$result = $conn->query($sql);
 			
 			echo '<div class="content">';
-			echo '<h2>Choose a doujin!</h2>';
+			echo '<h2>';
+			echo 'Choose a doujin!';
+			// Add search bar to search doujins by title
+			echo '<form method="post" style="float: right;">';
+				echo '<input type="text" name="title_search" placeholder="Search your doujin!" required>';
+				echo '&nbsp;&nbsp;';
+				echo '<input type="submit" value="Enter" class="mySearchButton">';
+			echo '</form>';
+			echo '</h2>';
 			echo '</div>';
 			
-			// Show all entries in doujins table in database
-			echo '<div class="grid-container">';
-				while($row = $result->fetch_assoc()) {
-					echo '<div class="doujin">';
-					echo '<img src="' . $row["image_directory"] . '" width="250" height="350";">';
-					echo '<div class="info">' . $row["title"] . ' [' . $row["artist"] . ']</div>';
-					echo '<form method="post">';	// This form button does nothing for now
-					echo '<input type="hidden" name="doujinNumber" value="' . $row['id'] . '">';
-					echo '<input type="submit" value="Add to doujin list" class="myButton">';
-					echo '</form>';
+			// If something was submitted through the title search bar
+			if (isset($_POST["title_search"])) {
+				// Find entries in doujins table in database where the title column contains input
+				$title_search = $_POST["title_search"];
+				$result = mysqli_query($conn, "SELECT * from doujins where title LIKE '%$title_search%'"); 
+			
+				// If result is found
+				if ($result->num_rows > 0) {
+					echo '<div class="grid-container">';
+						while($row = $result->fetch_assoc()) {					
+							echo '<div class="doujin">';
+							echo '<img src="' . $row["image_directory"] . '" width="250" height="350";">';
+							echo '<div class="info">' . $row["title"] . ' [' . $row["artist"] . ']</div>';
+							echo '<form method="post">';	// This form button does nothing for now
+							echo '<input type="hidden" name="doujinNumber" value="' . $row['id'] . '">';
+							echo '<input type="submit" value="Add to doujin list" class="myButton">';
+							echo '</form>';
+							echo '</div>';
+						}
 					echo '</div>';
 				}
-			echo '</div>';
+				else {
+					echo '<div style="text-align: center;">No results found!</div>';
+				}
+			}
+			else {
+				// When the page is just loaded
+				// Show all entries in doujins table in database
+				echo '<div class="grid-container">';
+					while($row = $result->fetch_assoc()) {					
+						echo '<div class="doujin">';
+						echo '<img src="' . $row["image_directory"] . '" width="250" height="350";">';
+						echo '<div class="info">' . $row["title"] . ' [' . $row["artist"] . ']</div>';
+						echo '<form method="post">';	// This form button does nothing for now
+						echo '<input type="hidden" name="doujinNumber" value="' . $row['id'] . '">';
+						echo '<input type="submit" value="Add to doujin list" class="myButton">';
+						echo '</form>';
+						echo '</div>';
+					}
+				echo '</div>';
+			}
+			
+			
 		?>
 	</body>
 </html>
