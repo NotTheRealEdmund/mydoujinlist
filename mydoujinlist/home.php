38ed490f55f9399f530ad5b5d18592cd77271ae8
nottherealedmund <nottherealedmund@gmail.com>
2021-07-22 13:28:57 +0800

    Add a button in order to add a doujin to the user's doujin list in future

--- mydoujinlist/home.php
+++ mydoujinlist/home.php
@@ -40,8 +40,10 @@ Available at: https://codeshack.io/secure-login-system-php-mysql/
 		.doujin {
 			border: 1px solid #ccc;
 			box-shadow: 2px 2px 6px 0px  rgba(0,0,0,0.3);
-			height: 400px;
+			height: 440px;
 			border-radius: 10px;
+			/*Required to make button stay at the bottom*/
+			position: relative;
 		}
 		.doujin img {
 		  	max-width: 100%;
@@ -52,6 +54,19 @@ Available at: https://codeshack.io/secure-login-system-php-mysql/
 		  	text-align: center;
 		  	font-size: 14px;
 		  	font-weight: 700;
+		}
+		.myButton {
+		  	cursor: pointer;
+			background: #e9658d;
+			border: 0;
+			color: white;
+		  	padding: 10px;
+		  	width: 100%;
+		  	border-bottom-left-radius: 10px;
+			border-bottom-right-radius: 10px;
+		  	/*Required to make button stay at the bottom*/
+		  	position: absolute;
+		  	bottom: 0;
 		}
 	</style>
 	<body class="loggedin">
@@ -85,6 +100,10 @@ Available at: https://codeshack.io/secure-login-system-php-mysql/
 					echo '<div class="doujin">';
 					echo '<img src="' . $row["image_directory"] . '" width="250" height="350";">';
 					echo '<div class="info">' . $row["title"] . ' [' . $row["artist"] . ']</div>';
+					echo '<form method="post">';	// This form button does nothing for now
+					echo '<input type="hidden" name="doujinNumber" value="' . $row['id'] . '">';
+					echo '<input type="submit" value="Add to doujin list" class="myButton">';
+					echo '</form>';
 					echo '</div>';
 				}
 			echo '</div>';
